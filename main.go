d97e931e0eaac023c47cb02a6369859c5d218161
Ferdiany19 <ferdianyuliansyah19@gmail.com>
2020-07-04 17:54:06 +0700

    static routing Handle

--- main.go
+++ main.go
@@ -16,6 +16,20 @@ func main() {
 		w.Write([]byte("Hello again"))
 	})
 
+	// Static Route file perlu didefiniskan
+	// rute-nya dan handler-nya
+
+	// Sedang untuk handler-nya bisa di-lihat,
+	// ada pada parameter ke-2 yang isinya statement http.StripPrefix().
+	// Sebenarnya actual handler nya berada pada http.FileServer(). Fungsi http.StripPrefix()
+	// hanya digunakan untuk membungkus actual handler.
+
+	// Fungsi http.StripPrefix() ini berguna untuk menghapus prefix dari endpoint yang di-request.
+
+	http.Handle("/static/",
+		http.StripPrefix("/static",
+			http.FileServer(http.Dir("assets"))))
+
 	fmt.Println("server started at localhost:9000")
 	http.ListenAndServe(":9000", nil)
 }
